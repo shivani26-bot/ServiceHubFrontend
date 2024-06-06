e88af38a5319343dae9dce18be0fbbf65cc6468d
Shivani <shivanihgf7@gmail.com>
2024-06-06 12:36:24 +0530

    port changed to 5173

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // change the default port to 3000
   server: {
-    port: 3000,
+    host: "0.0.0.0",
+    port: 5173,
   },
-})
+});
